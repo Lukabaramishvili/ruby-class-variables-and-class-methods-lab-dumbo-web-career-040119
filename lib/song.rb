917bf533b08e94984620abf20810cb897fe20008
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-06 21:59:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,10 @@ class Song
   
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each do ||
-    
+    @@genres.each do |genre|
+      genre_count[genre] += 1 
+      genre_count
+    end
   end 
   
   
