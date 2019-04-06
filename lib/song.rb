f8e6d5914254011f64aa75d7f8f5cb46bf842334
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-04-06 21:44:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,14 @@
 class Song 
 
-  attr_accessor :name, :artist, :genre
-    @@count = 0  
-    @@artists = []
-    @@genres = []
-  def initialize(name, artist, genre)
-    @name = name 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  
+  def initialize(name,artist,genre)
+    @name = name
     @artist = artist
     @genre = genre
     @@count += 1 
-    @@artists << artist
-    @@gernres << genre 
   end 
-  
-  def self.count
-    @@count
-  end 
-
-# def self.genres  
-#   @@genres.uniq 
-# end 
-
-# def self.artists  
-#   @@artists.uniq 
-# end 
   
 end 
